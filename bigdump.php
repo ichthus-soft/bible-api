bb8bac0bcded5dbc79b0659c3eb9d4244948f1ff
Filip <hello@filipac.net>
2014-07-30 17:37:33 +0300

    test

--- bigdump.php
+++ bigdump.php
@@ -31,7 +31,7 @@ $app->register(new \Silex\Provider\DoctrineServiceProvider(), array(
 ));
 
 $sql = "SHOW TABLES LIKE 'biblia'";
-if(count($app['db']->fetchAll($sql)) == 1) {
+if(count($app['db']->fetchAll($sql)) == 1 AND $_GET['start'] != '1') {
   // echo __DIR__;
   die('Baza de date a fost deja importata!');
 }
